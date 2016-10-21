af34852e32f5b945e9909d534ca7689ef1ef92f9
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2016-10-22 00:25:43 +0200

    Dodana implementacija sklada z vaj

--- vaje/vaje3.py
+++ vaje/vaje3.py
@@ -6,14 +6,15 @@ class StackRecord:
         Inicializacija vnosa v skladu.
         Časovna zahtevnost: O(1)
         """
-        pass
+        self.x = x
+        self.next = next
 
     def __repr__(self):
         """
         Znakovna predstavitev vnosa v skladu.
         Časovna zahtevnost: O(1)
         """
-        pass
+        return repr(self.x)
 
 class Stack:
     """Sklad (LIFO)"""
@@ -22,49 +23,63 @@ class Stack:
         Inicializacija sklada.
         Časovna zahtevnost: O(1)
         """
-        pass
+        self.clear()
 
     def __len__(self):
         """
         Velikost sklada.
         Časovna zahtevnost: O(1)
         """
-        pass
+        return self.len
 
     def __repr__(self):
         """
         Znakovna predstavitev sklada.
         Časovna zahtevnost: O(n)
         """
-        pass
+        cur = self.top
+        out = '<| '
+        while cur is not None:
+            out += '%s, ' % repr(cur)
+            cur = cur.next
+        return out + '#'
 
     def clear(self):
         """
         Izprazni sklad.
         Časovna zahtevnost: O(1)
         """
-        pass
+        self.len = 0
+        self.top = None
 
     def peek(self):
         """
         Vrni vrhnji element v skladu.
         Časovna zahtevnost: O(1)
         """
-        pass
+        if self.top is None:
+            raise IndexError("sklad je prazen")
+        return self.top.x
 
     def pop(self):
         """
         Odstrani in vrni vrhnji element v skladu.
         Časovna zahtevnost: O(1)
         """
-        pass
+        if self.top is None:
+            raise IndexError("sklad je prazen")
+        top = self.top
+        self.top = top.next
+        self.len -= 1
+        return top.x
 
     def push(self, x):
         """
         Dodaj element na vrh sklada.
         Časovna zahtevnost: O(1)
         """
-        pass
+        self.top = StackRecord(x, self.top)
+        self.len += 1
 
 class QueueRecord:
     """Vnos v vrsti"""
