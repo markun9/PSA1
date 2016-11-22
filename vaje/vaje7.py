2df872ebb1d628001f17a4aa1f3e4c100742871f
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2016-11-22 10:48:47 +0100

    Dodani komentarji k rešitvam

--- vaje/vaje7.py
+++ vaje/vaje7.py
@@ -11,6 +11,11 @@ from vaje5 import DFS
 from vaje6 import decompose, toporder
 
 def countPaths(G, s, t):
+    """
+    Prešteje poti od s do t v usmerjenem acikličnem grafu G.
+
+    Časovna zahtevnost: O(m)
+    """
     sez = toporder(G)
     poti = [0] * len(G)
     poti[s] = 1
@@ -20,14 +25,26 @@ def countPaths(G, s, t):
     return poti[t]
 
 def hamiltonianPath(G):
+    """
+    Poišče Hamiltonovo pot v usmerjenem acikličnem grafu, če ta obstaja.
+
+    Časovna zahtevnost: O(m)
+    """
     sez = toporder(G)
     for i in range(1, len(sez)):
         if sez[i] not in G[sez[i-1]]:
             return False
-    return True
+    return sez
 
 def cheapestReachable(G, p, DFS = DFS):
+    """
+    Za dani seznam cen vozlišč vrne seznam cen najcenejšega vozlišča,
+    ki je dosegljivo iz posameznega vozlišča usmerjenega grafa G.
+
+    Časovna zahtevnost: O(m)
+    """
     n = len(G)
+    assert n == len(p)
     komp, GL = decompose(G, DFS = DFS)
     seznam = [min(p[u] for u in k) for k in komp]
     for u in range(len(GL)):
