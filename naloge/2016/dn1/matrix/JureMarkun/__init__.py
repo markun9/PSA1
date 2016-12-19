c4b893658fb895f459fe802a7b2bfe67b07a04e9
markun9 <jure.markun@gmail.com>
2016-12-19 23:13:48 +0100

    Delete __init__.py

--- naloge/2016/dn1/matrix/JureMarkun/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Objekti, ki se uvozijo s from ... import *
-__all__ = ['SlowMatrix', 'FastMatrix', 'CheapMatrix']
-
-# Uvozimo naše razrede
-from .slowmatrix import SlowMatrix
-from .fastmatrix import FastMatrix
-from .cheapmatrix import CheapMatrix
